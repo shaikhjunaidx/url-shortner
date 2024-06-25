11e27ce3ee490981b75ab18776491d3ce3eaf5cf
shaikhjunaidx <shaikhjunaidx@gmail.com>
2024-06-25 16:27:11 -0400

    Initial files

---
+++ api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"github.com/shaikhjunaidx/url-shortner/routes"
+)
+
+func setUpRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+
+	err := godotenv.Load()
+	
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	setUpRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
---
+++ api/routes/shorten.go
@@ -0,0 +1,43 @@
+package routes
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
+	"github.com/shaikhjunaidx/url-shortner/helpers"
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Not a valid URL"})
+	}
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Domain error"})
+	}
+
+	body.URL = helpers.EnforceHTTP(body.URL)
+
+	
+}
